40a48cf366a650c1dbebc806f4174e9aa83948bd
Dave68858 <wrx65262@student.wroclaw.merito.pl>
2024-12-05 20:06:56 +0100

    LAB1

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/entity/PatientEntity.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/entity/PatientEntity.java
@@ -1,13 +1,10 @@
 package com.jpacourse.persistence.entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "PATIENT")
@@ -33,6 +30,13 @@ public class PatientEntity {
 
 	@Column(nullable = false)
 	private LocalDate dateOfBirth;
+
+	@OneToOne(cascade = CascadeType.ALL) // Jednostronna od strony właściciela
+	@JoinColumn(name = "address_id", referencedColumnName = "id", nullable = false)
+	private AddressEntity address;
+
+	@OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true) // Dwustronna relacja
+	private List<VisitEntity> visits = new ArrayList<>();
 
 	public Long getId() {
 		return id;
