bf790179e7fa0a4a9d71227d5a6ffffd50f37492
Dave68858 <wrx65262@student.wroclaw.merito.pl>
2024-12-05 21:57:08 +0100

    lab1

--- VisitEntity.java
+++ VisitEntity.java
@@ -22,6 +22,18 @@ public class VisitEntity {
 	@Column(nullable = false)
 	private LocalDateTime time;
 
+	@ManyToOne
+	@JoinColumn(name = "doctor_id", nullable = false) // Dwustronna relacja
+	private DoctorEntity doctor;
+
+	@ManyToOne
+	@JoinColumn(name = "patient_id", nullable = false) // Dwustronna relacja
+	private PatientEntity patient;
+
+	@ManyToOne // Jednostronna od strony właściciela
+	@JoinColumn(name = "treatment_id", nullable = true)
+	private MedicalTreatmentEntity treatment;
+
 	public Long getId() {
 		return id;
 	}
